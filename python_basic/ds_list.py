57703f321659890b8ec4c839f5e7930dfc75f885
Walden <walden980302@outlook.com>
2023-02-08 11:47:09 -0500

    first upload of python_basic

---
+++ python_basic/ds_list.py
@@ -0,0 +1,56 @@
+courses = ['History', 'Math', 'Physics', 'CompSci']
+nums = [1, 5, 2, 4, 3]
+
+# course_str = ' - '.join(courses)
+# print(course_str)
+# new_list = course_str.split(' - ')
+# print(new_list)
+
+# for index, course in enumerate(courses, start=1):
+#     print(index, course)
+
+# for item in courses:
+#     print(item)
+
+# find the index of the certain element
+# print(courses.index('CompSci'))
+# print('CompSci' in courses)
+
+# find max and min values, and sum,
+# print(max(nums))
+
+# return the sorted list but it actually 
+# does not sort the list itself
+# sorted_courses = sorted(courses)
+# print(sorted_courses)
+
+# sort (alphabatically)
+# courses.sort()
+# courses.sort(reverse=True)
+
+# reverse
+# courses.reverse()
+
+# remove
+# courses.remove('Math')
+# courses.pop()
+
+# print(len(courses))
+
+# last one will always be the last item
+# print(courses[-1])
+
+# [including, excluding]
+# print(courses[0:2])
+
+# append the item at the last
+# courses.append('Art')
+
+# insert the element at index location
+# courses.insert(0, 'Music')
+
+# extend
+# courses_2 = ['Art', 'Education']
+# courses.extend(courses_2)
+
+print(courses)
